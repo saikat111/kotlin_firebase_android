1d0ca4aec03c2ecda9c12075b64d6c3130debcbf
saikat111 <s8856010@gmail.com>
2022-06-30 12:53:42 +0600

    RecyclerView

---
+++ app/src/main/java/com/xm/TryActivity.kt
@@ -0,0 +1,12 @@
+package com.xm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xm.kotlintutorial.R
+
+class TryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_try)
+    }
+}
